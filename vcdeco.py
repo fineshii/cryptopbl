eed7f908fa4b5cb463a1ed8608410d260406effb
Priyanshu <priyanshuu2121@gmail.com>
2025-10-11 22:58:31 +0530

    Update vcdeco.py

--- vcdeco.py
+++ vcdeco.py
@@ -1,2 +1,14 @@
 import numpy as np
 from PIL import Image
+
+def reconstruct(shares_paths):
+    combined = None
+    for path in shares_paths:
+        img = Image.open(path).convert('1')
+        data = np.array(img) // 255
+        if combined is None:
+            combined = data
+        else:
+            combined ^= data  # XOR for reconstruction
+
+    Image.fromarray(combined * 255).show()
