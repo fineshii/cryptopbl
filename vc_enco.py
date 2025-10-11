292a5e3595ba552abf4b117db02374b9e58672b2
Priyanshu <priyanshuu2121@gmail.com>
2025-10-11 22:26:49 +0530

    Update vc_enco.py

--- vc_enco.py
+++ vc_enco.py
@@ -6,3 +6,11 @@ def genshare(imagepath,n,k):
   data=np.array(img)
   h,w=data.shape
   shares =[np.random.randint(0,2,(h,w),dtype=np.uint8) for _ in range(n)]
+encoded.data.copy()
+for i in range(k):
+  encoded^= shares[i]
+shares[k-1] = encoded
+os.makwdirs("assets/shares",exist_ok=True)
+for i,s in enumerate(shares):
+  Image.fromarray(s*255).save(f"assests/shares/share_{i+1}.png")
+printf(f"{n} shares generated with threshold k={k}")
